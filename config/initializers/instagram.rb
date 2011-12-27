a646e930cd9a7e284462f87cc1fca7ee4a0383fc
Masahiko Tachizono <mshk@andmore.jp>
2011-12-26 16:03:43 -0800

    updated initializers

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = "d3577ba1d32b4b429fcc94a775e7cdd9"
-  config.client_secret = "df261367095a4ab1b6c86f0836963a65"
+  config.client_id = "CLIENT_ID"
+  config.client_secret = "CLIENT_SECRET"
 end
 
 CALLBACK_URL = "http://localhost:3000/session/callback"
